a44fa9340c6804a52341c349ab18e4db69a18018
RockstarSteph <stephaniechreifi@gmail.com>
2019-05-07 20:45:31 +0000

    set EJS as view engine for HTML

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+// Setting the Express app to use EJS as its templating engine for the HTML
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
